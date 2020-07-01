4f882e3a72164ebfd028bf81bac45250f3a0d93f
James Begley <jmbegley@gmx.com>
2020-06-30 21:15:10 -0400

    added some additional tests/output

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,20 @@ using std::string;
 
 int main(int argc, char** argv) {
 	Logger("main entered.");
+	Logger("a = new Atom() -- create a regular, self-naming Atom.");
 	Atom* a = new Atom();
 	string id = a->GetIdentity();
-	string amsg = "a->GetIdentity() returned: \"" + id + "\".";
-	Logger(amsg.c_str());
+	string msg = "a->GetIdentity() returned: \"" + id + "\".";
+	Logger(msg.c_str());
+	Logger("deleting a...");
+	delete a;
+	Logger("b = new Atom(\"fancyname\") -- create an Atom with the identity \"fancyname\"");
+	Atom* b = new Atom("fancyname");
+	id = b->GetIdentity();
+	msg = "b->GetIdentity() returned: \"" + id + "\".";
+	Logger(msg.c_str());
+	Logger("deleting b...");
+	delete b;
 	Logger("that's about enough for today.");
 	return 0;
 }
