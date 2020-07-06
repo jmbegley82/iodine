365f7892b7edbb4af16256f27c8e1bd71c03f892
James Begley <jmbegley@gmx.com>
2020-07-06 17:51:24 -0400

    added Doxygen comments to Timing.h

--- src/Timing.h
+++ src/Timing.h
@@ -9,9 +9,32 @@
 extern "C" {
 #endif //__cplusplus
 
+/**
+ * @brief Get a timestamp representing the current time with millisecond granularity
+ * @details The starting point of the timestamp will depend upon library providing the function.
+ * @return double The timestamp in milliseconds
+ */
 double GetTimeInMsec();
+
+/**
+ * @brief Get a timestamp representing the current time with microsecond granularity
+ * @details The starting point of the timestamp will depend upon library providing the function.
+ * @return double The timestamp in microseconds
+ */
 double GetTimeInUsec();
+
+/**
+ * @brief Attempt to sleep for at least ms milliseconds
+ * @details Or more literally, call SleepUsec(ms*1000)
+ * @param ms (double) The amount of time to sleep, in milliseconds
+ */
 void SleepMsec(double ms);
+
+/**
+ * @brief Attempt to sleep for at least us microseconds
+ * @details Currently our sleeping is done by using the usleep function.
+ * @param us (int) The amount of time to sleep, in microseconds
+ */
 void SleepUsec(int us);
 
 #ifdef __cplusplus
