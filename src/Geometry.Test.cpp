f7e0883aead60a2ba93508464ea82720044ec71c
James Begley <jmbegley@gmx.com>
2020-07-09 23:55:32 -0400

    added more tests to Geometry.Test.cpp

--- src/Geometry.Test.cpp
+++ src/Geometry.Test.cpp
@@ -19,7 +19,19 @@ int Geometry::Test() {
 	double yt = g->GetYTop();
 	double yb = g->GetYBottom();
 	Logger("Geometry::Test:  position set to (100,100), size set to (36,48), center offset set to (-12,-24)");
-	sprintf(msg, "Geometry::Test:  XLeft=%f, XRight=%f, YTop=%f, YBottom=%f", xl, xr, yt, yb);
+	sprintf(msg, "Geometry::Test:  XLeft=%.1f, XRight=%.1f, YTop=%.1f, YBottom=%.1f", xl, xr, yt, yb);
+	Logger(msg);
+	coords pair = g->GetTopLeft();
+	sprintf(msg, "Geometry::Test:      Top Left corner X=%5.1f, Y=%5.1f", pair.x, pair.y);
+	Logger(msg);
+	pair = g->GetTopRight();
+	sprintf(msg, "Geometry::Test:     Top Right corner X=%5.1f, Y=%5.1f", pair.x, pair.y);
+	Logger(msg);
+	pair = g->GetBottomLeft();
+	sprintf(msg, "Geometry::Test:   Bottom Left corner X=%5.1f, Y=%5.1f", pair.x, pair.y);
+	Logger(msg);
+	pair = g->GetBottomRight();
+	sprintf(msg, "Geometry::Test:  Bottom Right corner X=%5.1f, Y=%5.1f", pair.x, pair.y);
 	Logger(msg);
 	return 0;
 }
