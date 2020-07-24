fa8cd4f2e18a68a5ccb4a1b24e4db65e1bd44947
James Begley <jmbegley@gmx.com>
2020-07-23 21:00:47 -0400

    added test for extra-long line passed to C++ Log(string)

--- src/Logger.Test.cpp
+++ src/Logger.Test.cpp
@@ -12,6 +12,10 @@ using std::string;
 int LogCxxTest() {
 	string msg = "LogCxxTest:  ";
 	Log(msg + "Log test passed!");
+	Log(msg + "The test where you pass an absurdly long string from the C++ part of Logger to the C part of Logger and "
+		+ "hope like hell that it chunks it up correctly and doesn't SEGFAULT and doesn't access invalid memory at "
+		+ "0xWherever, and also doesn't raid your address book and email client and sell the data to yada yada yada "
+		+ "etc. passed!");
 	Log_now(msg + "Log_now test passed!");
 	return 0;
 }
